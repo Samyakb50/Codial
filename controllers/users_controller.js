ce8009be28d9e910050ea405532bb137f525677a
Samyak Jain <Samyak.Jain@thalesgroup.com>
2021-10-14 21:46:58 +0530

    Added JWT Authentication

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,4 +1,6 @@
 const User = require('../models/user')
+const fs = require('fs')
+const path = require('path')
 
 module.exports.profile = function(req, res){
    
@@ -54,16 +56,45 @@ module.exports.create = function(req, res){
     });
 }
 
-module.exports.update = function(req, res){
+module.exports.update = async function(req, res){
+   
+
     if(req.user.id == req.params.id){
-        User.findByIdAndUpdate(req.params.id, req.body, function(err, user){
+
+        try{
+
+            let user = await User.findById(req.params.id);
+            User.uploadedAvatar(req, res, function(err){
+                if (err) {console.log('*****Multer Error: ', err)}
+                
+                user.name = req.body.name;
+                user.email = req.body.email;
+
+                if (req.file){
+
+                    if (user.avatar){
+                        fs.unlinkSync(path.join(__dirname, '..', user.avatar));
+                    }
+
+
+                    // this is saving the path of the uploaded file into the avatar field in the user
+                    user.avatar = User.avatarPath + '/' + req.file.filename;
+                }
+                user.save();
+                return res.redirect('back');
+            });
+
+        }catch(err){
+            req.flash('error', err);
             return res.redirect('back');
-        });
-    } else {
-        return res.status(401).isAuthenticated('Unauthorized');
+        }
+
+
+    }else{
+        req.flash('error', 'Unauthorized!');
+        return res.status(401).send('Unauthorized');
     }
 }
-
 //sign in & create session for user
 module.exports.createSession = function(req, res){
     req.flash('success', 'Logged in Successfully')
