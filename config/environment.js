f6860e56cf4e13f132a6529c36072717a5d6e961
Jain Samyak <T0241748@gemalto.com>
2023-09-08 23:55:49 +0530

    Changed environment variable callback_url

--- config/environment.js
+++ config/environment.js
@@ -18,7 +18,7 @@ const development = {
     },
     google_client_id: "974244660273-djpn0m0j42n6ahso2crjj8t8on5s9a90.apps.googleusercontent.com",
     google_client_secret: "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",
-    google_call_back_url: "http://codeial.live/users/auth/google/callback",
+    google_call_back_url: "http://localhost:8000/users/auth/google/callback",
     jwt_secret: 'codeial'
 }
 
@@ -40,7 +40,7 @@ const production =  {
     },
     google_client_id: process.env.CODEIAL_GOOGLE_CLIENT_ID,
     google_client_secret: process.env.CODEIAL_GOOGLE_CLIENT_SECRET,
-    google_call_back_url: process.env.CODEIAL_GOOGLE_CALLBACK_RURL,
+    google_call_back_url: process.env.CODEIAL_GOOGLE_CALLBACK_URL,
     jwt_secret: process.env.CODEIAL_JWT_SECRET
 }
 
