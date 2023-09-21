50ad320c67d30e2adc9e30f7a36c63d0889ea9ef
Samyak Jain <samyak.jain@thalesgroup.com>
2023-09-21 22:25:27 +0530

    Try

--- config/environment.js
+++ config/environment.js
@@ -40,7 +40,7 @@ const production =  {
     },
     google_client_id: process.env.CODEIAL_GOOGLE_CLIENT_ID,
     google_client_secret: process.env.CODEIAL_GOOGLE_CLIENT_SECRET,
-    google_call_back_url: process.env.CODEIAL_GOOGLE_CALLBACK_URL,
+    google_call_back_url: "http://codeial.live/users/auth/google/callback",
     jwt_secret: process.env.CODEIAL_JWT_SECRET
 }
 
